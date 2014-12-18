2da920e7779b0d590a48358f9d8096b1a84f4b21
jzdev <jzdev@outlook.com>
2014-12-17 21:01:53 -0500

    Updated migration

--- app/database/migrations/2014_12_16_004726_create_tables.php
+++ app/database/migrations/2014_12_16_004726_create_tables.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreateTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -82,5 +82,4 @@ class CreateUsersTable extends Migration {
 		Schema::drop('recipients');
 		Schema::drop('gifts');
 	}
-
 }
